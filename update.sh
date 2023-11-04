984c4dd65b227312a30b44e14f8df3a1f919de44
andriwibowo2222 <131835463+andriwibowo2222@users.noreply.github.com>
2023-11-05 01:44:37 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Kangsantri123/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/andriwibowo2222/silentisgold/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
